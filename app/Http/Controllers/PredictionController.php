c6be4542ab31ec8beba7437d97036b39fb1c8b54
Isaack Motanya <isaackmotanya6@gmail.com>
2021-02-09 14:59:11 +0300

    Changed port for flask api

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -31,7 +31,7 @@ class PredictionController extends Controller
 
         try {
             $client = new \GuzzleHttp\Client();
-            $endpoint = 'http://192.168.100.2:80/classify';
+            $endpoint = 'http://0.0.0.0:5000/classify';
             $file               = $request->image_to_test;
             $file_path          = $file->getPathname();
             $file_mime          = $file->getMimeType('image');
