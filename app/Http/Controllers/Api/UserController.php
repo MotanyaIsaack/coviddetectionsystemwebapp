fc237597acb8e3dbd1115ddf178af91cfbf856a6
Isaack Motanya <isaackmotanya6@gmail.com>
2021-02-09 11:14:29 +0300

    Removed invalid import for Validator class

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Prediction;
 use App\User;
-use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
