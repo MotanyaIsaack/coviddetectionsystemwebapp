c4784f90a01fcf76bac52ba4008ab2994a658c75
Isaack Motanya <isaackmotanya6@gmail.com>
2021-02-09 11:24:38 +0300

    Fixed the validator class for the UserController Api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Prediction;
 use App\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
